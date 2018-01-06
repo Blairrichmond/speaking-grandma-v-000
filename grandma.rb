7235f72bf5db4d0356c936f54551d6cfc972cf01
Brent  Ransom <sadumedusa@gmail.com>
2018-01-06 16:40:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(string)
-  if string == string.upcase
+  if string == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
   elsif string == 'Hi Nana, how are you?'
     return 'HUH?! SPEAK UP, SONNY!'
